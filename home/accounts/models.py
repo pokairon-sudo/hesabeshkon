a94ac5868b180d811fb1997af850e02618b0cd5e
pokairon-sudo <parhambagheri1001@gmail.com>
2025-12-06 14:05:06 +0000

    add group

--- home/accounts/models.py
+++ home/accounts/models.py
@@ -49,46 +49,11 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
-#
-# class UserManager(BaseUserManager):
-#     def create_user(self, username, email, password=None, phone=None):
-#         if not email:
-#             raise ValueError('The Email field must be set')
-#         if not username:
-#             raise ValueError('The Username field must be set')
-#
-#         email = self.normalize_email(email)
-#         user = self.model(username=username, email=email, phone=phone)
-#         user.set_password(password)  # Hash the password
-#         user.save(using=self._db)
-#         return user
-#
-#     def create_superuser(self, username, email, password=None, phone=None):
-#         user = self.create_user(username, email, password, phone)
-#         user.is_staff = True
-#         user.is_superuser = True
-#         user.save(using=self._db)
-#         return user
-#
-# class User(AbstractBaseUser, PermissionsMixin):
-#     username = models.CharField(max_length=150, unique=True)
-#     email = models.EmailField(unique=True)
-#     phone = models.CharField(max_length=15, blank=True, null=True)
-#
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#
-#     groups = models.ManyToManyField(Group, related_name='accounts_users', blank=True)
-#     user_permissions = models.ManyToManyField(Permission, related_name='accounts_user_permissions', blank=True)
-#
-#     objects = UserManager()
-#
-#     USERNAME_FIELD = 'email'  # Email will be the unique identifier
-#     REQUIRED_FIELDS = ['username']  # Required fields
-#
-#     def __str__(self):
-#         return self.email
-
+@receiver(post_save, sender=User)
+def add_user_to_customer_group(sender, instance, created, **kwargs):
+    if created:
+        customer_group, _ = Group.objects.get_or_create(name='customer')
+        instance.groups.add(customer_group)
