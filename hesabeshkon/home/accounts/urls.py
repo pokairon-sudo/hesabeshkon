0d76277eb4c15ee389bcf46e2e3dc7fb3248135d
pokairon-sudo <parhambagheri1001@gmail.com>
2026-09-22 21:39:15 +0330

    Delete hesabeshkon directory

--- hesabeshkon/home/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserRegisterView, LoginView, LogoutView, ProfileView
-
-urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-]
